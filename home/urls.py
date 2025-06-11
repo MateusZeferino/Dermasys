e8f0e4070fbf1f44ebf2bda4cbb750db9b2257bf
MateusZeferino <mateuszjanuario@hotmail.com>
2025-06-11 18:01:01 -0300

    Arrumando o front-end - Telas de login / cadastro do cliente - tatuador(a) - arrumado as urls / hrefs / campos de input

--- home/urls.py
+++ home/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.login, name='login'),
-    path('cadastro/cliente/', views.cadastro_cliente, name="cadastro_cliente"),
-    path('cadastro/tatuador/', views.cadastro_tatuador, name="cadastro_tatuador")
+    path('cadastro-cliente/', views.cadastro_cliente, name="cadastro_cliente"),
+    path('cadastro-tatuador/', views.cadastro_tatuador, name="cadastro_tatuador")
 ]
